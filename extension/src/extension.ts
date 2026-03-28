52a3680d57f62844bce8082615e701c30fe2febe
Youssef Nabil <146959536+youssefnabil2030@users.noreply.github.com>
2026-03-28 13:51:57 -0700

    Implement language client for Python server

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,1 +1,26 @@
+import * as path from 'path';
+import { workspace, ExtensionContext } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node';
 
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+    // مسار ملف البايثون
+    const serverPath = context.asAbsolutePath(path.join('server', 'server.py'));
+    
+    const serverOptions: ServerOptions = {
+        command: 'python', 
+        args: [serverPath],
+    };
+
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: 'file', language: 'python' }],
+    };
+
+    client = new LanguageClient('azureGuardian', 'Azure SDK Guardian', serverOptions, clientOptions);
+    client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+    return client ? client.stop() : undefined;
+}
